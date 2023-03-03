e1281435c32d9d1963829d772e7e6638a9a2174f
plotiwitolp <plotiwitolp@gmail.com>
2023-03-03 19:16:43 +0400

    v1

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="<?php bloginfo('language'); ?>">
+
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php bloginfo('name'); ?></title>
+    <?php wp_head(); ?>
+</head>
+
+<body>
+    <header>
+        header
+    </header>
